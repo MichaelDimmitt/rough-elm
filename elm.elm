4be4c877d1cfe058b5f23f523d32cd93a77684b3
michaeldimmitt <michaelgdimmitt@gmail.com>
2018-07-07 14:59:43 -0400

    multiple buttons with notes.

--- elm.elm
+++ elm.elm
@@ -1,13 +1,17 @@
+-- lookup : auto currying
 import Html
 import Html.Attributes
 import Html.Events
 
+-- type Small5List a = (a,a,a,a,a)
+
 type alias Model
-  = Bool
+  = List Bool
 
 -- algerbraic data type.
+-- int being the index of the button.
 type Msg 
-  = Click
+  = Click Int
 
 main : Program Never Model Msg
 main =
@@ -19,29 +23,47 @@ main =
   -- myButton 
 
 myModel : Model
+
+-- change the model to whatever you want and let the compiler guide you to future values.
+-- when is the right time to break out the model and the view.
+-- dont couple a view to a model.
+-- typically we let the data drive the application.
 myModel = 
-  True
+  [False, False, True, False]
+
+myView : Model -> Html.Html Msg
+myView model = 
+  Html.div [] (List.indexedMap myButtonView model)
 
 -- Render the state of the application as HTML
 
 -- Render state of application as Html
-myView : Model -> Html.Html Msg
-myView x = 
+myButtonView : Int -> Bool -> Html.Html Msg
+myButtonView i button = 
   Html.div
     []
     [
       Html.button 
-        [Html.Events.onClick Click] 
+        [Html.Events.onClick (Click i)] 
         [ Html.text "Click me"]
       , Html.text 
-        ( if x  == True 
+        ( if button  == True 
           then "Goodbye" 
           else "Hello")
     ]
 
 myUpdate : Msg -> Model -> Model 
 -- take the view and the old model and return a new model which is not the old model
-myUpdate x y = not y
+myUpdate msg model = 
+  case msg of 
+    Click i -> 
+    List.indexedMap (updateButton i) model
+
+-- does type inference the entire time you dont have to declare the types but 
+-- if you tried to change a type incorrectly it would throw an error.
+updateButton : Int -> Int -> Bool -> Bool
+updateButton i index button =
+ if i == index then not button else button
 
 -- double dash signifies a comment.
 -- button no longer used.
