797604a54a443de21eff3e6fef45686f14aea868
michaeldimmitt <michaelgdimmitt@gmail.com>
2018-07-07 14:02:45 -0400

    basic program, says hello.

--- elm.elm
+++ elm.elm
@@ -1,10 +1,37 @@
 import Html
 
-main : Html.Html msg
+
+type alias Model
+  = Bool
+
+-- algerbraic data type.
+type Msg 
+  = Something
+
+main : Program Never Model Msg
 main =
-  myButton 
+  Html.beginnerProgram
+    { model  = myModel
+    , view   = myView
+    , update = myUpdate
+    }
+  -- myButton 
 
+myModel : Model
+myModel = 
+  True
 
+-- Render the state of the application as HTML
+myView : Model -> Html.Html Msg
+myView x = Html.text "Hello" 
+
+myUpdate : Msg -> Model -> Model 
+myUpdate x y = y
+
+-- double dash signifies a comment.
+-- button no longer used.
+
+-- main : Html.Html msg
 myButton : Html.Html msg
 myButton = Html.button [] myInnerArray
 
